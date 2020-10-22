a132be7d3be1caea744717a2c2d671aeb25efc8f
ShaikhAbdulSami <shabdulsami4@gmail.com>
2020-10-22 13:33:47 +0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import  React from 'react';
 import './App.css';
 import LaunchContainer from './components/Launches/index';
 import LaunchDetailsContainer from './components/LaunchDetails/index';
-import { BrowserRouter as Router,  Route } from 'react-router-dom'
+import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <div className='main'>
         <h1>Welcome to Space X Application</h1>
-        
+        <Routes>
           {/* main root path */}
           <Route path='/' element={<LaunchContainer />} />
           {/* indiviual connection  to launches*/}
           <Route path='launch/:id' element={<LaunchDetailsContainer />} />
-        
+        </Routes>
+
       </div>
     </Router>
   );
